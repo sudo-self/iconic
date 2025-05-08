ac7a0bbec47934fc0eb657d5e61c1c1dfe41582d
Sudo-Self <sudo-self@proton.me>
2025-05-08 09:21:55 -0600

    Update top-carousel.tsx

--- components/top-carousel.tsx
+++ components/top-carousel.tsx
@@ -7,7 +7,7 @@ const examples = [
   "https://pub-c1de1cb456e74d6bbbee111ba9e6c757.r2.dev/image3.svg",
   "https://pub-c1de1cb456e74d6bbbee111ba9e6c757.r2.dev/image-5.svg",
   "https://pub-c1de1cb456e74d6bbbee111ba9e6c757.r2.dev/prompt1.svg",
-  "https://pub-c1de1cb456e74d6bbbee111ba9e6c757.r2.dev/image-6.svg",
+  "https://pub-c1de1cb456e74d6bbbee111ba9e6c757.r2.dev/image-9.svg",
   "https://pub-c1de1cb456e74d6bbbee111ba9e6c757.r2.dev/image-7.svg",
   "https://pub-c1de1cb456e74d6bbbee111ba9e6c757.r2.dev/image-8.svg"
 ]
