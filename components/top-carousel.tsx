5a5817e9ec0d60cf1a6413368ac52e1c6d09b4b0
Sudo-Self <sudo-self@proton.me>
2025-05-08 00:52:56 -0600

    Update top-carousel.tsx

--- components/top-carousel.tsx
+++ components/top-carousel.tsx
@@ -4,11 +4,14 @@ import { useEffect, useState } from "react"
 
 const examples = [
   "https://pub-c1de1cb456e74d6bbbee111ba9e6c757.r2.dev/cyberpunk.svg",
-  "https://pub-c1de1cb456e74d6bbbee111ba9e6c757.r2.dev/prompt1.svg",
   "https://pub-c1de1cb456e74d6bbbee111ba9e6c757.r2.dev/image3.svg",
   "https://pub-c1de1cb456e74d6bbbee111ba9e6c757.r2.dev/image-5.svg",
+  "https://pub-c1de1cb456e74d6bbbee111ba9e6c757.r2.dev/prompt1.svg",
   "https://pub-c1de1cb456e74d6bbbee111ba9e6c757.r2.dev/image-6.svg",
+  "https://pub-c1de1cb456e74d6bbbee111ba9e6c757.r2.dev/image-7.svg",
+  "https://pub-c1de1cb456e74d6bbbee111ba9e6c757.r2.dev/image-8.svg"
 ]
+
 
 export default function TopCarousel() {
   const [isPaused, setIsPaused] = useState(false)
