b37b693d9a6f8aff6165723efe65375d8f851c75
Sudo-Self <sudo-self@proton.me>
2025-05-10 18:51:39 -0600

    Update top-carousel.tsx

--- components/top-carousel.tsx
+++ components/top-carousel.tsx
@@ -9,7 +9,7 @@ const examples = [
   "https://pub-c1de1cb456e74d6bbbee111ba9e6c757.r2.dev/prompt1.svg",
   "https://pub-c1de1cb456e74d6bbbee111ba9e6c757.r2.dev/image-10.svg",
   "https://pub-c1de1cb456e74d6bbbee111ba9e6c757.r2.dev/image%20(2).svg",
-  "https://pub-c1de1cb456e74d6bbbee111ba9e6c757.r2.dev/image-8.svg"
+  "https://pub-c1de1cb456e74d6bbbee111ba9e6c757.r2.dev/image-12.svg"
 ]
 
 export default function TopCarousel() {
