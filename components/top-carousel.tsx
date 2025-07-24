9eaec6ba99fa3ed91a7c738b5d1a6348171b0eff
sudo-self <sudo-self@proton.me>
2025-07-24 04:02:02 -0600

    Update top-carousel.tsx

--- components/top-carousel.tsx
+++ components/top-carousel.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react"
 const examples = [
 
   "https://firebasestorage.googleapis.com/v0/b/svetle-book.appspot.com/o/iconic%2F1.png?alt=media&token=fd7f62de-40c9-4ea5-a63b-2ff917cf6e65",
-  "https://firebasestorage.googleapis.com/v0/b/svetle-book.appspot.com/o/iconic%2Ficon-256x256.png?alt=media&token=be5f2f80-802f-4be3-bc44-f31f02a75435",
+  "https://firebasestorage.googleapis.com/v0/b/svetle-book.appspot.com/o/iconic%2Fsksksksksks.png?alt=media&token=c88f8c26-bafe-438e-a2ba-75434c422a3f",
   "https://firebasestorage.googleapis.com/v0/b/svetle-book.appspot.com/o/iconic%2Fgolden.png?alt=media&token=70b26b84-dab8-45f9-8439-e90987539392",
   "https://firebasestorage.googleapis.com/v0/b/svetle-book.appspot.com/o/iconic%2F111111.png?alt=media&token=a201bafa-7b5c-4546-b187-d53f624fea6e",
   "https://firebasestorage.googleapis.com/v0/b/svetle-book.appspot.com/o/iconic%2F5555555.png?alt=media&token=1fe2c5f0-dbd2-4199-806a-90f25ce20e16",
