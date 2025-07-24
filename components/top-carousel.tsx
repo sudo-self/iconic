1d55aa752c41e13d5a5a7dca64c91cc45986e8f7
sudo-self <sudo-self@proton.me>
2025-07-24 03:43:21 -0600

    Update top-carousel.tsx

--- components/top-carousel.tsx
+++ components/top-carousel.tsx
@@ -9,7 +9,7 @@ const examples = [
   "https://firebasestorage.googleapis.com/v0/b/svetle-book.appspot.com/o/iconic%2F2.png?alt=media&token=5c1dca2d-6a24-4781-a3b1-d7e471252aac",
   "https://firebasestorage.googleapis.com/v0/b/svetle-book.appspot.com/o/iconic%2Fimage-3.webp?alt=media&token=855cd0c3-8d22-4814-8251-1ca2e1822a6e",
   "https://firebasestorage.googleapis.com/v0/b/svetle-book.appspot.com/o/iconic%2FCover.jpg?alt=media&token=c20c92e1-3aea-414b-995c-4c40206dec80",
-  "https://firebasestorage.googleapis.com/v0/b/svetle-book.appspot.com/o/iconic%2F2d.png?alt=media&token=7c74fc88-de9c-444c-90d1-2bd42cae358a",
+  "https://firebasestorage.googleapis.com/v0/b/svetle-book.appspot.com/o/iconic%2F789.png?alt=media&token=8fd40bed-2d82-4ed4-b02b-21993f0a05e3",
   "https://firebasestorage.googleapis.com/v0/b/svetle-book.appspot.com/o/iconic%2Ficon-512x512.png?alt=media&token=6e1cb487-342e-4d47-8df3-a168e3963f94",
   "https://firebasestorage.googleapis.com/v0/b/svetle-book.appspot.com/o/iconic%2F4.png?alt=media&token=341633d1-447e-4517-acb1-e8969089d79f"
 ]
