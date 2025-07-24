319639f81115c0d6271bdc1ea7f795ff49316617
sudo-self <sudo-self@proton.me>
2025-07-23 22:31:59 -0600

    Update top-carousel.tsx

--- components/top-carousel.tsx
+++ components/top-carousel.tsx
@@ -3,19 +3,19 @@
 import { useEffect, useState } from "react"
 
 const examples = [
-  // Neon
+
   "https://firebasestorage.googleapis.com/v0/b/svetle-book.appspot.com/o/iconic%2Ficon-512x512.png?alt=media&token=1eccfbcc-d2f7-4a51-b3ad-c4d443b7e245", // neon Tokyo
   "https://firebasestorage.googleapis.com/v0/b/svetle-book.appspot.com/o/iconic%2FTablerBrandThreejs.svg?alt=media&token=f1573d23-e521-4c21-bab5-184fe2cf4b74", // neon alley
 
-  // Black and White
+  
   "https://firebasestorage.googleapis.com/v0/b/svetle-book.appspot.com/o/iconic%2FCover.jpg?alt=media&token=c20c92e1-3aea-414b-995c-4c40206dec80", // BW portrait
   "https://firebasestorage.googleapis.com/v0/b/svetle-book.appspot.com/o/iconic%2F2d.png?alt=media&token=7c74fc88-de9c-444c-90d1-2bd42cae358a", // BW man
 
-  // Cartoon / Anime
+ 
   "https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/male/45.png",
   "https://firebasestorage.googleapis.com/v0/b/svetle-book.appspot.com/o/iconic%2FIconParkApple.svg?alt=media&token=f197b3f9-0620-44ab-9f88-86ebf5710f45",
 
-  // Bonus examples
+ 
   "https://firebasestorage.googleapis.com/v0/b/svetle-book.appspot.com/o/iconic%2Ficon-512x512.png?alt=media&token=3839cd7a-ff65-4d0a-80ab-7cf5ce7c3784", // surreal
   "https://firebasestorage.googleapis.com/v0/b/svetle-book.appspot.com/o/iconic%2FIMG_0494.png?alt=media&token=e8a3b3dd-b240-46ee-b63c-2d1f58ac23d8"  // neon portrait
 ]
