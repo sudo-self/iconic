66505f177d0e7220884c7172cd237313fffabadb
sudo-self <sudo-self@proton.me>
2025-07-23 22:34:48 -0600

    Update top-carousel.tsx

--- components/top-carousel.tsx
+++ components/top-carousel.tsx
@@ -17,7 +17,7 @@ const examples = [
 
  
   "https://firebasestorage.googleapis.com/v0/b/svetle-book.appspot.com/o/iconic%2Ficon-512x512.png?alt=media&token=3839cd7a-ff65-4d0a-80ab-7cf5ce7c3784", // surreal
-  "https://firebasestorage.googleapis.com/v0/b/svetle-book.appspot.com/o/iconic%2FIMG_0494.png?alt=media&token=e8a3b3dd-b240-46ee-b63c-2d1f58ac23d8"  // neon portrait
+  "https://firebasestorage.googleapis.com/v0/b/svetle-book.appspot.com/o/iconic%2Fimage-3.webp?alt=media&token=855cd0c3-8d22-4814-8251-1ca2e1822a6e"  // neon portrait
 ]
 
 export default function TopCarousel() {
